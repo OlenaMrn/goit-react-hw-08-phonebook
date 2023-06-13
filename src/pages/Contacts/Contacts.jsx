d4b604c4cdc96a752ce109ede5bd5bcdb7dc7582
Olena <anyanyme@gmail.com>
2023-06-13 15:36:27 +0200

    update Contacts

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,22 +1,22 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { fetchContacts } from '../../redux/contacts/contactsOperations';
-import {
-  selectContacts,
-  selectError,
-  selectIsLoading,
-} from '../../redux/contacts/contactsSelectors';
+// import {
+//   selectContacts,
+//   selectError,
+//   selectIsLoading,
+// } from '../../redux/contacts/contactsSelectors';
 
 import { ContactForm } from '../../components/ContactForm/ContactForm';
-import { Filter } from '../../components/Filter/Filter';
+// import { Filter } from '../../components/Filter/Filter';
 // import { ContactList } from '../../components/ContactList/ContactList';
 
 import css from './Contacts.module.css';
 
 const Contacts = () => {
-  const contacts = useSelector(selectContacts);
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+//   const contacts = useSelector(selectContacts);
+//   const isLoading = useSelector(selectIsLoading);
+//   const error = useSelector(selectError);
   const dispatch = useDispatch();
 
   useEffect(() => {
